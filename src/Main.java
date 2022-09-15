6c06949a7d5bda97e72519111c9f695f87547156
Oleg Ti <Olegos250387@Gmail.com>
2022-09-15 23:01:35 +0300

    git commit -m "mili"

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 500;
+        int ru = 20;
+        int bonus = amount/ru;
+        System.out.println("Бонус составит "+bonus+" miles");
+
+
+
+
+    }
+}
